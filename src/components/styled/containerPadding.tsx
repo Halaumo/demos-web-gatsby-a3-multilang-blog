68359ec2eb6d7e046680079acd796776708f24f9
Artyom <Artyom@example.com>
2021-02-28 18:50:36 +0600

    styles 2

--- src/components/styled/containerPadding.tsx
+++ src/components/styled/containerPadding.tsx
@@ -3,11 +3,6 @@ import { css } from 'styled-components'
 const containerPadding = css`
   padding-left: 10px;
   padding-right: 10px;
-
-  /* @media screen and (max-width: 767px){
-    padding-left: 8px;
-    padding-right: 8px;
-  }; */
 `
 
 export default containerPadding
